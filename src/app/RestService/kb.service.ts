44f1137d7473909139b96d022fbad6f8d6e24223
Heber Garmendia Lara <gheber_@hotmail.com>
2021-07-01 13:48:08 -0500

    my first commit message

---
+++ src/app/RestService/kb.service.ts
@@ -0,0 +1,7 @@
+import{Injectable} from '@angular/core'
+
+@Injectable(){
+    export class KbService{
+        console.log('Servicio inicializado')
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContadorComponent } from './contador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule
